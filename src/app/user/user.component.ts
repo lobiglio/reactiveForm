760c5f56284c526d90722110337e2e32ea766951
letizia obiglio <letizia.obiglio@gmail.com>
2019-12-02 11:14:41 +0100

    quest 15 done

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
 import { User } from "./../models/user";
 import { Component, OnInit } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { FormControl, FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-user",
@@ -8,24 +8,33 @@ import { FormControl } from "@angular/forms";
   styleUrls: ["./user.component.scss"]
 })
 export class UserComponent implements OnInit {
-  username = new FormControl("");
-  useremail = new FormControl("");
-  userpassword = new FormControl("");
-  userstreet = new FormControl("");
-  userpostalcode = new FormControl("");
-  usercity = new FormControl("");
+  userForm = this.fb.group({
+    username: [""],
+    credentials: this.fb.group({
+      useremail: [""],
+      userpassword: [""]
+    }),
+    userstreet: [""],
+    userpostalcode: [""],
+    usercity: [""]
+  });
+
   user: User = new User();
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {}
 
-  createUser() {
-    this.user.name = this.username.value;
-    this.user.email = this.useremail.value;
-    this.user.password = this.userpassword.value;
-    this.user.address.street = this.userstreet.value;
-    this.user.address.postalCode = this.userpostalcode.value;
-    this.user.address.city = this.usercity.value;
+  // createUser() {
+  //   this.user.name = this.username.value;
+  //   this.user.email = this.useremail.value;
+  //   this.user.password = this.userpassword.value;
+  //   this.user.address.street = this.userstreet.value;
+  //   this.user.address.postalCode = this.userpostalcode.value;
+  //   this.user.address.city = this.usercity.value;
+  // }
+
+  onSubmit() {
+    console.log(this.userForm.value);
   }
 }
